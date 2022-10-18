d52814445af0ba504cb671bbf57f273774674f6f
Divy Srivastava <dj.srivastava23@gmail.com>
2022-10-18 10:35:11 +0530

    a

--- generate_comment.js
+++ generate_comment.js
@@ -1,21 +1,42 @@
-const repo = Deno.args[0] || "denoland/deno";
+import "https://deno.land/std/dotenv/load.ts";
+
+const repo = Deno.args[0] || "littledivy/equinix-metal-test";
 const pullNumber = Deno.args[1] || "1";
 const token = Deno.env.get("GITHUB_TOKEN");
 
+const osDir = Deno.build.os === "linux" ? "linux64" : "mac";
+const hyperfine = `third_party/prebuilt/${osDir}/hyperfine`;
+
 async function generateComment() {
-    const comment = {
-        body: `Hello, world!`,
-    };
-    const response = await fetch(
-        `https://api.github.com/repos/${repo}/pulls/${pullNumber}/reviews`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `token ${token}`,
-            },
-            body: JSON.stringify(comment),
-        },
-    );
-    return response.json();
+  const comment = {
+    body: await Deno.readTextFile("benchmark.md"),
+  };
+  const response = await fetch(
+    `https://api.github.com/repos/${repo}/issues/${pullNumber}/comments`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `token ${token}`,
+      },
+      body: JSON.stringify(comment),
+    },
+  );
+  return response.json();
 }
+
+async function runHyperfine() {
+  const result = await Deno.run({
+    cmd: [
+      hyperfine,
+      "--export-markdown",
+      "benchmark.md",
+      "deno run --allow-net --allow-env nop.js",
+      "node nop.js",
+    ],
+  });
+  await result.status();
+}
+
+await runHyperfine();
+console.log(await generateComment());
--- main.sh
+++ main.sh
@@ -3,7 +3,6 @@
 apt-get install unzip git
 curl -fsSL https://deno.land/install.sh | sh
 
-git clone --depth=1 https://github.com/denoland/deno_third_party
-git clone --depth=1 https://github.com/littledivy/equinix-metal-test
+git clone --depth=1 --recurse-submodules https://github.com/littledivy/equinix-metal-test
 
 ~/.deno/bin/deno run -A --unstable equinix-metal-test/generate_comment.js
