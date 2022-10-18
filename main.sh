cac43295f27a87a82080044ee5cfe2146832aef9
Divy Srivastava <dj.srivastava23@gmail.com>
2022-10-18 14:14:48 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,3 +6,4 @@ curl -fsSL https://deno.land/install.sh | sh
 git clone --depth=1 --recurse-submodules https://github.com/littledivy/equinix-metal-test
 
 ~/.deno/bin/deno run -A --unstable equinix-metal-test/generate_comment.js
+
